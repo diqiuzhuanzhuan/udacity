446213e49ada998bf53b259e24949ff0d0d6b87b
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2017-07-12 15:37:25 +0800

    默认训练的代码中，Q表中键值对的创建有bug，已存在的部分会被覆盖为0。造成的结果是训练效果一直振荡，永远不会收敛。

--- projects_cn/smartcab-master/smartcab/agent.py
+++ projects_cn/smartcab-master/smartcab/agent.py
@@ -100,8 +100,8 @@ class LearningAgent(Agent):
         #   Then, for each action available, set the initial Q-value to 0.0
         if not self.learning:
             return
-
-        self.Q[state] = dict((j, 0.0) for j in self.valid_actions)
+        if state not in self.Q:
+            self.Q[state] = dict((j, 0.0) for j in self.valid_actions)
         return
 
 
@@ -194,7 +194,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, log_metrics=True)
+    sim = Simulator(env, update_delay=0.01, log_metrics=True, optimized=False)
     
     ##############
     # Run the simulator
