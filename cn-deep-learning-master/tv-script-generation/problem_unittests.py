b52e7fbec371120ab3e2fca0ce66996b3a1db659
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2017-09-19 11:36:22 +0800

    1. 完成RNN模型的自动生成剧本。

--- cn-deep-learning-master/tv-script-generation/problem_unittests.py
+++ cn-deep-learning-master/tv-script-generation/problem_unittests.py
@@ -169,6 +169,7 @@ def test_get_inputs(get_inputs):
 def test_get_init_cell(get_init_cell):
     with tf.Graph().as_default():
         test_batch_size_ph = tf.placeholder(tf.int32)
+        test_batch_size_ph = 128
         test_rnn_size = 256
 
         cell, init_state = get_init_cell(test_batch_size_ph, test_rnn_size)
@@ -236,7 +237,8 @@ def test_build_nn(build_nn):
         test_embed_dim = 300
         test_rnn_layer_size = 2
         test_vocab_size = 27
-        test_cell = rnn.MultiRNNCell([rnn.BasicLSTMCell(test_rnn_size)] * test_rnn_layer_size)
+        
+        test_cell = rnn.MultiRNNCell([rnn.BasicLSTMCell(test_rnn_size) for _ in  range(test_rnn_layer_size)])
 
         logits, final_state = build_nn(test_cell, test_rnn_size, test_input_data, test_vocab_size, test_embed_dim)
 
